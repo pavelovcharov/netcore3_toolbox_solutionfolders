d7060f867732ebfbcd202f109c69725460ba29ac
Ovcharov.Pavel <1357165+pavelovcharov@users.noreply.github.com>
2019-10-24 19:08:00 +0300

    Add project files.

---
+++ WpfApp46/TestControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace TestControl {
+    public class TestControl : ContentControl {
+        public TestControl() {
+            DefaultStyleKey = typeof(TestControl);
+        }
+    }
+}
